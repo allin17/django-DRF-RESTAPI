dc26d0be352327454eba049bf2250201b9e9796d
allin17 <zhernosek.zahar@mail.ru>
2023-04-05 15:01:07 +0300

    initial

---
+++ drfsite/women/serializers.py
@@ -0,0 +1,16 @@
+import io
+
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+
+from women.models import Women
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
