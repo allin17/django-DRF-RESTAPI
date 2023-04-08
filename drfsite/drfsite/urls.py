3ab2ad926895c622769bd07a3f12e2a03fe0a7e5
allin17 <zhernosek.zahar@mail.ru>
2023-04-08 16:00:55 +0300

    create custom router && add overwriting method get_queryset

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -19,8 +19,24 @@ from rest_framework import routers
 
 from women.views import *
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get': 'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix': 'List'}),
+        routers.Route(url=r'^{prefix}/{lookup}$',
+                      mapping={'get': 'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix': 'Detail'})
+    ]
+
+
+router = MyCustomRouter()
+router.register(r'women', WomenViewSet, basename='women')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from women.models import Women
+from women.models import Women, Category
 from women.serializers import WomenSerializer
 
 
@@ -8,9 +10,20 @@ from women.serializers import WomenSerializer
 
 
 class WomenViewSet(viewsets.ModelViewSet):
-    queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+
+        if not pk:
+            return Women.objects.all()[:3]
+
+        return Women.objects.filter(pk=pk)
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
 # class WomenAPIList(generics.ListCreateAPIView):
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
