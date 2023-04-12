d3ab7bc98f9eeb13c5ea691f2caa32f03fe04e9e
allin17 <ivanovzahar1000@gmail.com>
2023-04-12 13:14:19 +0300

    add pagination in settings.py && create pagination class

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
@@ -12,10 +13,17 @@ from women.serializers import WomenSerializer
 # Create your views here.
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 3
+
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = WomenAPIListPagination
 
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
